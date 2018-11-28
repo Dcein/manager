ec6f80b37cd3a40f4beb2c28482bf7aed9b33cb6
dingcongcong <dcein520@163.com>
2018-11-28 19:45:04 +0800

    add readme file and add pom file

---
+++ common-module/src/main/java/com/ding/common/api/Test.java
@@ -0,0 +1,11 @@
+package com.ding.common.api;
+
+/**
+ * @program: manager
+ * @description: 测试接口
+ * @author: DingCong
+ * @create: 2018-11-28 19:37
+ **/
+public class Test {
+
+}
---
+++ resource-module/src/main/java/com/ding/resource/controller/ApplicationTest.java
@@ -0,0 +1,10 @@
+package com.ding.resource.controller;
+
+/**
+ * @program: manager
+ * @description: 应用测试
+ * @author: DingCong
+ * @create: 2018-11-28 19:35
+ **/
+public class ApplicationTest {
+}
