7fab7f107e78013351c9ff0a95f952c29a886ea1
dingcongcong <dcein520@163.com>
2018-12-11 13:08:36 +0800

    commit

--- common-module/src/main/java/com/ding/common/dao/SysMenuMapper.java
+++ common-module/src/main/java/com/ding/common/dao/SysMenuMapper.java
@@ -1,7 +1,9 @@
 package com.ding.common.dao;
 
 import com.ding.common.entity.SysMenu;
+import org.springframework.stereotype.Repository;
 import tk.mybatis.mapper.common.Mapper;
 
+@Repository
 public interface SysMenuMapper extends Mapper<SysMenu> {
 }
